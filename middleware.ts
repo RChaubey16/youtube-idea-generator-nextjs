312fdd6dbe98229e22ea807c135fdb5641df5958
RChaubey16 <ruturajchaubey16@gmail.com>
2025-03-03 16:14:24 +0530

    dev: Add middleware which check user's auth state and allows them access to certain routes.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// Route matcher for the user. If the user's on '/sign-in' then this will be TRUE.
+const isPublicRoute = createRouteMatcher(["/", "/sign-in"]);
+
+// Middleware to check if a user is authenticated before allowing them to access certain routes.
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  // Logged in user will have a userId meaning that the user is authenticated.
+  if (!isPublicRoute(req) && !userId) {
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
